5d5400c2bd43895b93104585f1b2fd9f3389a383
Milan <milanvpage@gmail.com>
2021-05-26 20:34:10 -0700

    commit tree ad83bd18898338360e7538b588bf54e65f4631b2

--- app.rb
+++ app.rb
@@ -2,4 +2,20 @@ require_relative 'config/environment'
 
 class App < Sinatra::Base
 
+    get '/' do
+        erb :index
+    end
+
+    get '/new' do
+        erb :create_puppy
+    end
+
+    post '/puppy' do
+        @puppy = Puppy.new(params[:name], params[:breed], params[:age])
+        # @puppy.name = params[:name]
+        # @puppy.breed = params[:breed]
+        # @puppy.age = params[:age]
+        erb :display_puppy
+    end
+
 end
